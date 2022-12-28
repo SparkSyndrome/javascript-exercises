57256612ccd9cabda48d0dc45c7ddb2116bc1497
Christopher Burton <christopherburton1020@gmail.com>
2022-12-28 15:14:04 -0700

    Write code and pass test for 'Repeats the string'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+let repeatedString = "";
 
+const repeatString = function(str, numRepeats) {
+  while (numRepeats > 0) {
+    repeatedString += str;
+    numRepeats--;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
