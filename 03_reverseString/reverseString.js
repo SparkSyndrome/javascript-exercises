e1e8f9482e55ea77d467be76725fd7a689df3a3c
Christopher Burton <christopherburton1020@gmail.com>
2022-12-28 16:26:06 -0700

    Write initial code. Pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let reversed = "";
+  for (i = str.length - 1; reversed.length < str.length; i--) {
+    reversed += str[i];
+  }
+  return reversed
 };
 
 // Do not edit below this line
