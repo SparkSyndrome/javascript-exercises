528cdc043f2ec1336ea3291f209a4394c09518da
Christopher Burton <christopherburton1020@gmail.com>
2022-12-28 15:26:32 -0700

    Write code and pass 'return ERROR w/ neg num test'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,14 @@
 const repeatString = function(str, numRepeats) {
   let repeatedString = "";
-  while (numRepeats > 0) {
-    repeatedString += str;
-    numRepeats--;
+  if (numRepeats < 0) {
+    return 'ERROR'
+  } else {
+    while (numRepeats > 0) {
+      repeatedString += str;
+      numRepeats--;
+    }
+    return repeatedString;
   }
-  return repeatedString;
 };
 
 // Do not edit below this line
