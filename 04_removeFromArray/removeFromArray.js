917ad4eae735edbe60b560b3330c6555b2f70efd
Christopher Burton <christopherburton1020@gmail.com>
2022-12-28 20:06:04 -0700

    Write initial code and pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, toBeRemoved) {
+  const indexToRemove = arr.indexOf(toBeRemoved);
+  arr.splice(indexToRemove, 1);
+  return arr;
 };
 
 // Do not edit below this line
