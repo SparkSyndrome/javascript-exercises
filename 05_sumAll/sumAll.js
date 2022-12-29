75e1f5f976bcec9aef590ce8c356836a9b71ffb2
Christopher Burton <christopherburton1020@gmail.com>
2022-12-28 21:17:27 -0700

    Rewrite code to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,23 @@
 const sumAll = function(min, max) {
   let sum = 0;
-  if (min > max) {
-    for (i = max; i <= min; i++) {
-      sum += i;
-    }
-    return sum;
-  } else {
-    for (i = min; i <= max; i++) {
-      sum += i;
-    }
-    return sum;
+  switch (true) {
+    case (min < 0 || max < 0):
+      return 'ERROR';
+    case (typeof min !== "number" || typeof max !== "number"):
+      return 'ERROR';
+  
+    default:
+      if (min > max) {
+        for (i = max; i <= min; i++) {
+          sum += i;
+        }
+        return sum;
+      } else {
+        for (i = min; i <= max; i++) {
+          sum += i;
+        }
+        return sum;
+      }
   }
 };
 
