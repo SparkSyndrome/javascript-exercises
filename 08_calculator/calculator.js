8c49c551630b59a00c773f21f95ca525ef6e4b92
Christopher Burton <christopherburton1020@gmail.com>
2023-02-24 21:10:20 -0700

    Pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const initialVal = 0;
+  summedArr = arr.reduce((total, num) => total += num, initialVal);
+  return summedArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  multipliedArr = arr.reduce((total, num) => total *= num);
+  return multipliedArr;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    return;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
