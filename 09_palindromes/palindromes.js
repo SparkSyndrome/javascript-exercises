4fbe5cc92e4eb73771db47b21c3dc10202dd0f43
Christopher Burton <christopherburton1020@gmail.com>
2023-02-24 22:20:26 -0700

    Pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Strip all non-alphabetic characters & whitespace, convert all chars to lowercase
+  const stripStr = str.replace(/\W+/g, "").toLowerCase();
+  // Convert stripped string to array, reverse the array, and convert back to string
+  const reverseStripStr = stripStr.split("").reverse().join("");
 
+  // Check if stripped string is palindrome w/ the reversed stripped string
+  if (stripStr === reverseStripStr) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
