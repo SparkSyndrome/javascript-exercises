651a8bab75486eb6ce9aff8b5585d7a53935a895
Christopher Burton <christopherburton1020@gmail.com>
2023-02-28 00:59:02 -0700

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  const fibNums = [1, 1];
+  let nextNum;
 
+  for (let i = 0; i < num; i++) {
+    nextNum = fibNums[i] + fibNums[i + 1];
+    fibNums.push(nextNum);
+  }
+
+  if (num >= 0) {
+    return fibNums[num - 1];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
